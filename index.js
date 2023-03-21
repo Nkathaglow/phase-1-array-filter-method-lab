b43c68821794477a13ce37586da667f6ccfc044b
Nkatha_Glory <kinyua6945@gmail.com>
2023-03-21 13:04:59 +0300

    Wueh

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+const driver = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby'];
+
+function findMatching(driver, names) {
+    return driver.filter(i => i.toLowerCase() == names.toLowerCase());
+}
+
+function fuzzyMatch(drivers, names){
+    let newDrivers = drivers.filter(i =>{
+        let letters = "Sa"
+        for (let letter = 0; letters < names.length; letter ++){
+            letters += i[letters];
+        }
+        return letters == names
+    })
+    return newDrivers
+}
+
+function matchName(driver, names){
+    let newDrivers = driver.filter(i => i.name == names)
+    return newDrivers;
+}
